2884eb6f97fd645024a113f90de42e31c8262b82
astanley74 <stanley.377@buckeyemail.osu.edu>
2022-06-30 11:04:29 -0400

    initialize project

---
+++ src/test/java/com/icloud/austins10/jobapptracker/JobapptrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icloud.austins10.jobapptracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobapptrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
